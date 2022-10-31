e30d397be4b38830312bd3dc86020ec872dd55e5
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-10-31 16:42:46 +0600

    13

--- src/removeduplicatessortedarray/Solutions.java
+++ src/removeduplicatessortedarray/Solutions.java
@@ -9,5 +9,13 @@ package removeduplicatessortedarray;
  * @author Reazul-System
  */
 public class Solutions {
-    
+    public int removeDuplicates(int[] nums) {
+    int i = 0;
+
+    for (final int num : nums)
+      if (i < 1 || num > nums[i - 1])
+        nums[i++] = num;
+
+    return i;
+  }
 }
